1e843fe0ed41b07349d2bd9de021f41df936b6ed
Amine <algusamine@gmail.com>
2021-07-14 00:41:10 -0400

    add-eventListeners

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     fetchCrimes()
+    //biasMotivationEvent()
 })
 
 function fetchCrimes(){
@@ -13,13 +14,18 @@ function fetchCrimes(){
 function getCrime (bias){
    console.log(bias)
 
-   // Filter out unique biasMotivation******
+// Filter out unique biasMotivation******
+
         let biasMotivation = [...new Set(bias.map(x => x.bias_motive_description))];
         //console.log(biasMotivation)
-    // put the unique items on a drop down list and append them to a div
+         // put the unique items on a drop down list and append them to a div
         let select = document.createElement('select')
         select.name = 'drop-down';
         select.id = 'drop-down';
+        //add event listener to select:
+        select.addEventListener('click', (e)=>{
+            console.log(e)
+            })
         biasMotivation.forEach((vectim)=>{
         let option = document.createElement('option');
         option.value = 'val'
@@ -27,35 +33,22 @@ function getCrime (bias){
         select.append(option)
 
          })
-    // append the div to the main div with the id year
+        // append the div to the main div with the id Motive-Description
         document.querySelector('#Motive-Description').append(select);
+        
 
-    // filter by year********
-        let year = [...new Set(bias.map(x => x.complaint_year_number))];
-        //console.log(year)
-    // put the year on a drop down list and append them to a div year
-        let selectYear = document.createElement('select')
-        selectYear.name = 'drop-down2';
-        selectYear.id = 'drop-down2';
-        year.forEach((vectimYear)=>{
-        let optionYear = document.createElement('option');
-        optionYear.value = 'val'
-        optionYear.textContent = vectimYear;
-        selectYear.append(optionYear)
-    // add an eventListner to select: 
-
-         })
-    // append the div to the main div with the id year
-        document.querySelector('#year').append(selectYear);
-    
-        // filter by county**********
+// filter by county**********
 
         let county = [...new Set(bias.map(x => x.county))];
-        console.log(county)
-    // put the county on a drop down list and append them to a div county
+        //console.log(county)
+        // put the county on a drop down list and append them to a div county
         let selectCounty = document.createElement('select')
         selectCounty.name = 'drop-down3';
         selectCounty.id = 'drop-down3';
+        //add event listener to selectCounty:
+        selectCounty.addEventListener('click', (e)=>{
+            console.log(e)
+            })
         county.forEach((vectimCounty)=>{
         let optionCounty = document.createElement('option');
         optionCounty.value = 'val'
@@ -63,13 +56,64 @@ function getCrime (bias){
         selectCounty.append(optionCounty)
 
          })
-    // append the div to the main div with the id county
+        // append the div to the main div with the id county
         document.querySelector('#county').append(selectCounty);
 
-    //submit button*****
+// filter by law code category descriptoin****
+
+        let felony = [...new Set(bias.map(x => x.law_code_category_description))];
+        //console.log(county)
+        // put the county on a drop down list and append them to a div felony
+        let selectFelony = document.createElement('select')
+        selectFelony.name = 'drop-down3';
+        selectFelony.id = 'drop-down3';
+        //add event listener to selectFelony:
+        selectFelony.addEventListener('click', (e)=>{
+            console.log(e)
+            })
+        felony.forEach((vectimFelony)=>{
+        let optionFelony = document.createElement('option');
+        optionFelony.value = 'val'
+        optionFelony.textContent = vectimFelony;
+        selectFelony.append(optionFelony)
+
+        })
+        // append the div to the main div with the id felony
+        document.querySelector('#felony').append(selectFelony);
+
+// filter by year********
+
+        let year = [...new Set(bias.map(x => x.complaint_year_number))];
+        //console.log(year)
+        // put the year on a drop down list and append them to a div year
+        let selectYear = document.createElement('select')
+        selectYear.name = 'drop-down2';
+        selectYear.id = 'drop-down2';
+        //add event listener to selectYear:
+        selectYear.addEventListener('click', (e)=>{
+            console.log(e)
+            })
+        year.forEach((vectimYear)=>{
+        let optionYear = document.createElement('option');
+        optionYear.value = 'val'
+        optionYear.textContent = vectimYear;
+        selectYear.append(optionYear)
+
+         })
+        // append the div to the main div with the id year
+        document.querySelector('#year').append(selectYear);
+    
+//submit button*****
+         
         let button = document.createElement('button');
         button.id = 'search';
         button.textContent = 'Search'
         document.querySelector('#search').append(button);
-        
-  }
+        // add eventListener to search button: 
+        button.addEventListener('click', (e)=>{
+            console.log(e)
+            })
+
+
+}
+
